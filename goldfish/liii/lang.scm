0887c6a63ca6fea04213f6c1b745130568fe2d00
jackyansongli <yansong@liii.pro>
2025-05-02 16:56:06 +0800

    add rich-vector%flat-map

--- goldfish/liii/lang.scm
+++ goldfish/liii/lang.scm
@@ -1226,6 +1226,9 @@
 (chained-define (%map x)
   (rich-vector (vector-map x data)))
 
+(chained-define (%flat-map f)
+  (rich-vector ((rich-vector (vector-map f data)) :reduce vector-append)))
+
 (chained-define (%filter x)
   (rich-vector (vector-filter x data)))
 
