a0c13709da8f5f3cc7a4f21c8516b596101c84f6
jackyansongli <yli340@uic.edu>
2025-05-20 19:17:40 +0800

    add datetime%plus-days

--- tests/goldfish/liii/datetime-test.scm
+++ tests/goldfish/liii/datetime-test.scm
@@ -32,3 +32,60 @@
 (check ((datetime :year 2025 :month 1 :day 1 :micro-second 999999) :to-string)
   => "2025-01-01 00:00:00.999999")
 
+;; Test plus-days with positive days
+(check ((datetime :year 2024 :month 1 :day 1) :plus-days 10) 
+  => (datetime :year 2024 :month 1 :day 11))
+
+(check ((datetime :year 2024 :month 1 :day 31) :plus-days 1) 
+  => (datetime :year 2024 :month 2 :day 1))
+
+(check ((datetime :year 2024 :month 1 :day 1) :plus-days 31) 
+  => (datetime :year 2024 :month 2 :day 1))
+
+(check ((datetime :year 2024 :month 2 :day 28) :plus-days 1) 
+  => (datetime :year 2024 :month 2 :day 29)) ; 2024 is a leap year
+
+(check ((datetime :year 2024 :month 2 :day 29) :plus-days 1) 
+  => (datetime :year 2024 :month 3 :day 1))
+
+(check ((datetime :year 2023 :month 2 :day 28) :plus-days 1) 
+  => (datetime :year 2023 :month 3 :day 1)) ; 2023 is not a leap year
+
+(check ((datetime :year 2024 :month 12 :day 31) :plus-days 1) 
+  => (datetime :year 2025 :month 1 :day 1))
+
+(check ((datetime :year 2024 :month 1 :day 1) :plus-days 366) 
+  => (datetime :year 2025 :month 1 :day 1)) ; 2024 is a leap year
+
+;; Test plus-days with negative days
+(check ((datetime :year 2024 :month 1 :day 11) :plus-days -10) 
+  => (datetime :year 2024 :month 1 :day 1))
+
+(check ((datetime :year 2024 :month 2 :day 1) :plus-days -1) 
+  => (datetime :year 2024 :month 1 :day 31))
+
+(check ((datetime :year 2024 :month 3 :day 1) :plus-days -1) 
+  => (datetime :year 2024 :month 2 :day 29))  ; 2024 is a leap year
+
+(check ((datetime :year 2023 :month 3 :day 1) :plus-days -1) 
+  => (datetime :year 2023 :month 2 :day 28))  ; 2023 is not a leap year
+
+(check ((datetime :year 2025 :month 1 :day 1) :plus-days -1) 
+  => (datetime :year 2024 :month 12 :day 31))
+
+(check ((datetime :year 2025 :month 1 :day 1) :plus-days -365) 
+  => (datetime :year 2024 :month 1 :day 2)) ; 2024 is a leap year
+
+;; Test plus-days with zero
+(check ((datetime :year 2024 :month 1 :day 1) :plus-days 0) 
+  => (datetime :year 2024 :month 1 :day 1))
+
+;; Test preserving time components
+(let ((dt (datetime :year 2024 :month 1 :day 1 
+                   :hour 12 :minute 30 :second 45 :micro-second 123456)))
+  (check (dt :plus-days 10) 
+    => (datetime :year 2024 :month 1 :day 11 
+                :hour 12 :minute 30 :second 45 :micro-second 123456)))
+
+(check-report)
+
