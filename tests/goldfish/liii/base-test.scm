750dffb9f71564a327e03fef73d9a80974a014d6
fzw-codelearner <3498363975@qq.com>
2025-07-11 09:20:37 +0000

    !542 [201_3] complex?的文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -448,6 +448,30 @@ boolean?
 (check-false (number? 'symbol))     ; 符号
 (check-false (number? '(1 2 3)))    ; 列表
 
+#|
+complex?
+判断一个对象是否是复数（包括整数、浮点数、有理数、复数）。
+
+语法
+----
+(complex? obj)
+
+参数
+----
+obj : any
+    任意类型的对象。
+
+返回值
+-----
+boolean?
+    如果 obj 是数值类型（整数、浮点数、有理数、复数）返回 #t，否则返回 #f。
+
+错误
+----
+无错误情况。
+
+|#
+
 (check-true (complex? 1+2i))        ; 复数
 (check-true (complex? 123))         ; 整数也是复数
 (check-true (complex? 123.456))     ; 浮点数也是复数
