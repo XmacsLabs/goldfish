55bca00cf3863c093474568dc36b44f846295dc3
fzw-codelearner <3498363975@qq.com>
2025-07-13 07:42:21 +0000

    !552 [201_3] rational?的文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -483,7 +483,7 @@ boolean?
 
 #|
 real?
-判断一个对象是否是有理数（包括整数、浮点数、有理数）。
+判断一个对象是否实数（包括整数、浮点数、有理数）。
 
 语法
 ----
@@ -513,6 +513,31 @@ boolean?
 (check-false (real? #t))            ; 布尔值
 (check-false (real? 'symbol))       ; 符号
 (check-false (real? '(1 2 3)))      ; 列表
+
+#|
+rational?
+判断一个对象是否是有理数（包括整数、有理数）。
+
+语法
+----
+(rational? obj)
+
+参数
+----
+obj : any
+    任意类型的对象。
+
+返回值
+-----
+boolean?
+    如果 obj 是数值类型（整数、有理数）返回 #t，否则返回 #f。
+
+错误
+----
+无错误情况。
+
+|#
+
 (check-true (rational? 123))        ; 整数
 (check-true (rational? 1/2))        ; 有理数
 (check-false (rational? 123.456))   ; 浮点数
