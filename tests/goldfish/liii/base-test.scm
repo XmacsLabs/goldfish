c5095ca2bb6348b299d9980c8789b51f3a965e1b
fzw-codelearner <3498363975@qq.com>
2025-07-14 03:13:52 +0000

    !553 [201_3] integer?的文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -546,6 +546,31 @@ boolean?
 (check-false (rational? #t))        ; 布尔值
 (check-false (rational? 'symbol))   ; 符号
 (check-false (rational? '(1 2 3)))  ; 列表
+
+#|
+integer?
+判断一个对象是否是整数（包括整数）。
+
+语法
+----
+(integer? obj)
+
+参数
+----
+obj : any
+    任意类型的对象。
+
+返回值
+-----
+boolean?
+    如果 obj 是数值类型（整数）返回 #t，否则返回 #f。
+
+错误
+----
+无错误情况。
+
+|#
+
 (check-true (integer? 123))         ; 整数
 (check-false (integer? 123.456))    ; 浮点数
 (check-false (integer? 1/2))        ; 有理数
