946bb733b1ade7e7a2d94354b6db766aa17b42eb
fzw-codelearner <3498363975@qq.com>
2025-07-22 08:46:54 +0000

    !579 [201_3] 添加truncate的文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -1225,12 +1225,41 @@ wrong-number-of-args
 (check (s7-ceiling 1.1) => 2)
 (check (s7-ceiling -1.2) => -1)
 
+#|
+truncate
+返回在靠近零的方向上最靠近给定数的整数。
+
+语法
+----
+(truncate num )
+
+参数
+----
+num : real?
+实数
+
+返回值
+------
+返回在靠近零的方向上最靠近给定数的整数，即正数向下取整，负数向上取整
+如果参数中存在不精确值，返回值也是不精确的，否则返回值是精确的
+
+错误
+----
+wrong-type-arg
+如果参数不是实数，抛出错误。
+wrong-number-of-args
+如果参数数量不为一，抛出错误。
+|#
+
 (check (truncate 1.1) => 1.0)
 (check (truncate 1) => 1)
 (check (truncate 1/2) => 0)
 (check (truncate 0) => 0)
 (check (truncate -1) => -1)
 (check (truncate -1.2) => -1.0)
+(check-catch 'wrong-type-arg (truncate 2+4i))
+(check-catch 'wrong-type-arg (truncate 'hello'))
+(check-catch 'wrong-number-of-args (truncate 4 5))
 
 (check (s7-truncate 1.1) => 1)
 (check (s7-truncate -1.2) => -1)
