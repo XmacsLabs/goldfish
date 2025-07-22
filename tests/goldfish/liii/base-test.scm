f4760e037d9fd795194bee3a2221231719fb9075
fzw-codelearner <3498363975@qq.com>
2025-07-22 08:24:29 +0000

    !576 [201_3] 添加floor和ceiling的文档和错误测试

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -1148,15 +1148,69 @@ wrong-number-of-args
 (check (* -9223372036854775808 9223372036854775807) => -9223372036854775808)
 (check (* -9223372036854775808 -9223372036854775808) => 0)
 
+#|
+floor
+返回不大于给定数的最大整数。
+
+语法
+----
+(floor num )
+
+参数
+----
+num : real?
+实数
+
+返回值
+------
+返回不大于给定数的最大整数
+如果参数中存在不精确值，返回值也是不精确的，否则返回值是精确的
+
+错误
+----
+wrong-type-arg
+如果参数不是实数，抛出错误。
+wrong-number-of-args
+如果参数数量不为一，抛出错误。
+|#
+
 (check (floor 1.1) => 1.0)
 (check (floor 1) => 1)
 (check (floor 1/2) => 0)
 (check (floor 0) => 0)
 (check (floor -1) => -1)
 (check (floor -1.2) => -2.0)
-
+(check-catch 'wrong-type-arg (floor 2+4i))
+(check-catch 'wrong-type-arg (floor 'hello'))
+(check-catch 'wrong-number-of-args (floor 4 5))
 (check (s7-floor 1.1) => 1)
 (check (s7-floor -1.2) => -2)
+
+#|
+ceiling
+返回不小于给定数的最小整数。
+
+语法
+----
+(ceiling num )
+
+参数
+----
+num : real?
+实数
+
+返回值
+------
+返回不小于给定数的最小整数
+如果参数中存在不精确值，返回值也是不精确的，否则返回值是精确的
+
+错误
+----
+wrong-type-arg
+如果参数不是实数，抛出错误。
+wrong-number-of-args
+如果参数数量不为一，抛出错误。
+|#
 
 (check (ceiling 1.1) => 2.0)
 (check (ceiling 1) => 1)
@@ -1164,6 +1218,9 @@ wrong-number-of-args
 (check (ceiling 0) => 0)
 (check (ceiling -1) => -1)
 (check (ceiling -1.2) => -1.0)
+(check-catch 'wrong-type-arg (ceiling 2+4i))
+(check-catch 'wrong-type-arg (ceiling 'hello'))
+(check-catch 'wrong-number-of-args (ceiling 4 5))
 
 (check (s7-ceiling 1.1) => 2)
 (check (s7-ceiling -1.2) => -1)
