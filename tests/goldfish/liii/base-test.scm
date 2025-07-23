427d6df304e83cee13b35794fe53cb192c55a6b8
Yuki <yifan@liii.pro>
2025-07-23 13:40:22 +0000

    !605 [201_2] 完成 R7RS 等价谓词 eqv?, eq?, equal? 的文档和测试 * [201_2] 完成 R7RS 等价谓词 eqv?, eq?, equal? 的文档和测试

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -2173,3 +2173,166 @@ wrong-type-arg
 
 (let1 port (open-input-string "ERROR")
   (check-catch 'wrong-type-arg (get-output-string port)))
+
+;; R7RS Equivalence Predicates Tests
+;; According to 201_2 task: eqv?, eq?, equal?
+
+#|
+eqv?
+判断两个对象是否值相等，根据R7RS规范，eqv?在不同类型的数据上表现不同。
+
+语法
+----
+(eqv? obj1 obj2)
+
+参数
+----
+obj1, obj2 : any
+任意类型的对象
+
+返回值
+-----
+boolean?
+如果两个对象值相等则返回 #t，否则返回 #f。
+
+|#
+
+;; Test eqv? for boolean values
+(check-true (eqv? #t #t))
+(check-true (eqv? #f #f))
+(check-false (eqv? #t #f))
+
+;; Test eqv? for exact numbers
+(check-true (eqv? 42 42))
+(check-false (eqv? 42 43))
+
+;; Test eqv? for inexact numbers
+(check-true (eqv? 3.14 3.14))
+(check-false (eqv? 3.14 2.71))
+
+;; Test eqv? for characters
+(check-true (eqv? #\a #\a))
+(check-false (eqv? #\a #\b))
+
+;; Test eqv? for symbols
+(check-true (eqv? 'abc 'abc))
+(check-false (eqv? 'abc 'def))
+
+;; Test eqv? for lists (same instance)
+(check-true (let ((lst (list 1 2 3)))
+              (eqv? lst lst)))
+
+;; Test eqv? for lists (different instances)
+(check-false (eqv? (list 1 2 3) (list 1 2 3)))
+
+;; Test eqv? for strings (always #f due to different instances)
+(check-false (eqv? "hello" "hello"))
+(check-false (eqv? "hello" "world"))
+
+;; Test eqv? for procedures
+(check-true (eqv? car car))
+(check-false (eqv? car cdr))
+
+;;; eq?
+
+#|
+eq?
+判断两个对象是否引用相同（对象为同一），即判断对象标识。
+
+语法
+----
+(eq? obj1 obj2)
+
+参数
+----
+obj1, obj2 : any
+任意类型的对象
+
+返回值
+-----
+boolean?
+如果两个对象是同一对象则返回 #t，否则返回 #f。
+|#
+
+;; Test eq? for boolean values
+(check-true (eq? #t #t))
+(check-true (eq? #f #f))
+(check-false (eq? #t #f))
+
+;; Test eq? for exact numbers (may return #f for different instances)
+(check-true (eq? 42 42))
+(check-false (eq? 42 43))
+
+;; Test eq? for symbols
+(check-true (eq? 'abc 'abc))
+(check-false (eq? 'abc 'def))
+
+;; Test eq? for lists (not the same instance)
+(check-false (eq? (list 1 2 3) (list 1 2 3)))
+(check-true (let ((lst (list 1 2 3)))
+              (eq? lst lst)))
+
+;; Test eq? for strings (always #f due to different instances)
+(check-false (eq? "hello" "hello"))
+
+;; Test eq? for procedures
+(check-true (eq? car car))
+(check-false (eq? car cdr))
+
+;;; equal?
+
+#|
+equal?
+判断两个对象结构是否相等，根据R7RS规范，equal?对复杂数据结构进行深比较。
+
+语法
+----
+(equal? obj1 obj2)
+
+参数
+----
+obj1, obj2 : any
+任意类型的对象
+
+返回值
+-----
+boolean?
+如果两个对象结构相等则返回 #t，否则返回 #f。
+|#
+
+;; Test equal? for simple types
+(check-true (equal? #t #t))
+(check-true (equal? 42 42))
+(check-true (equal? 3.14 3.14))
+(check-true (equal? "hello" "hello"))
+(check-true (equal? 'abc 'abc))
+
+;; Test equal? for lists
+(check-true (equal? (list 1 2 3) (list 1 2 3)))
+(check-false (equal? (list 1 2 3) (list 1 2 4)))
+
+;; Test equal? for nested lists
+(check-true (equal? (list (list 1 2) (list 3 4)) (list (list 1 2) (list 3 4))))
+(check-false (equal? (list (list 1 2) (list 3 4)) (list (list 1 2) (list 3 5))))
+
+;; Test equal? for vectors
+(check-true (equal? (vector 1 2 3) (vector 1 2 3)))
+(check-false (equal? (vector 1 2 3) (vector 1 2 4)))
+
+;; Test equal? for nested vectors
+(check-true (equal? (vector (vector 1 2) (vector 3 4)) (vector (vector 1 2) (vector 3 4))))
+(check-false (equal? (vector (vector 1 2) (vector 3 4)) (vector (vector 1 2) (vector 3 5))))
+
+;; Test equal? for mixed structures
+(check-true (equal? (list 1 (vector 2 3) 4) (list 1 (vector 2 3) 4)))
+(check-false (equal? (list 1 (vector 2 3) 4) (list 1 (vector 2 4) 4)))
+
+;; Test equal? for empty structures
+(check-true (equal? (list) (list)))
+(check-true (equal? (vector) (vector)))
+
+;; Test equal? for different types
+(check-false (equal? 42 "hello"))
+(check-false (equal? #\a "a"))
+
+(check-report)
