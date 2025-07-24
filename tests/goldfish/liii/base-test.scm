0c2906ace76acf7176686f5facffe6f64a692700
沈达 <da@liii.pro>
2025-07-24 12:57:55 +0000

    !628 [201_7] 完善symbol?的测试用例和文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -1624,46 +1624,6 @@ wrong-type-arg
 (check-false (boolean=? #t #f))
 (check-false (boolean=? #f #t))
 
-(check-true (symbol? 'foo))
-(check-true (symbol? (car '(foo bar))))
-(check-true (symbol? 'nil))
-
-(check-false (symbol? "bar"))
-(check-false (symbol? #f))
-(check-false (symbol? '()))
-(check-false (symbol? '123))
-
-(check-catch 'wrong-number-of-args (symbol=? 'a))
-(check-catch 'wrong-number-of-args (symbol=? 1))
-
-(check-true (symbol=? 'a 'a))
-(check-true (symbol=? 'foo 'foo))
-(check-false (symbol=? 'a 'b))
-(check-false (symbol=? 'foo 'bar))
-
-(check-true (symbol=? 'bar 'bar 'bar))
-
-(check-true (symbol=? (string->symbol "foo") (string->symbol "foo")))
-(check-false (symbol=? (string->symbol "foo") (string->symbol "bar")))
-
-(check-false (symbol=? 1 1))
-(check-false (symbol=? 'a 1))
-(check-false (symbol=? (string->symbol "foo") 1))
-
-(check-false (symbol=? 'a 'b '()))
-
-(check (symbol->string `MathAgape) => "MathAgape")
-(check (symbol->string 'goldfish-scheme) => "goldfish-scheme")
-
-(check (symbol->string (string->symbol "MathApage")) => "MathApage")
-(check (symbol->string (string->symbol "Hello World")) => "Hello World")
-
-(check (string->symbol "MathAgape") => `MathAgape)
-(check-false (equal? (string->symbol "123") '123))
-(check (string->symbol "+") => '+)
-
-(check (string->symbol (symbol->string `MathAgape)) => `MathAgape)
-
 (check (char? #\A) => #t)
 (check (char? 1) => #f)
 
@@ -2003,6 +1963,112 @@ wrong-type-arg
 (check (member "1" '(0 "1" 2 3)) => '("1" 2 3))
 (check (member '(1 . 2) '(0 (1 . 2) 3)) => '((1 . 2) 3))
 (check (member '(1 2) '(0 (1 2) 3)) => '((1 2) 3))
+
+
+#|
+symbol?
+判断给定的对象是否为符号(symbol)类型
+
+语法
+----
+(symbol? obj)
+
+参数
+----
+obj : any
+任意类型的对象
+
+返回值
+-----
+boolean?
+如果obj是符号类型则返回#t，否则返回#f
+
+说明
+----
+符号是Scheme中的基本数据类型之一，用单引号(')前缀表示。
+符号在Scheme中通常用作标识符、关键字或枚举值。
+
+|#
+
+(check-true (symbol? 'foo))
+(check-true (symbol? (car '(foo bar))))
+(check-true (symbol? 'nil))
+
+(check-false (symbol? "bar"))
+(check-false (symbol? #f))
+(check-false (symbol? '()))
+(check-false (symbol? '123))
+
+;; 边界情况测试
+(check-true (symbol? '+))
+(check-true (symbol? '-))
+(check-true (symbol? '*))
+(check-true (symbol? '/))
+(check-true (symbol? '==))
+(check-true (symbol? '=>))
+
+;; 数字开头符号测试
+(check-true (symbol? '123abc))
+(check-true (symbol? '1a2b3c))
+
+;; 空符号名称测试 (注意：某些scheme系统可能不支持空符号)
+(check-true (symbol? (string->symbol "empty-symbol")))
+
+;; 特殊符号测试
+(check-true (symbol? 'if))
+(check-true (symbol? 'lambda))
+(check-true (symbol? 'define))
+(check-true (symbol? 'let))
+(check-true (symbol? 'begin))
+
+;; 特殊符号格式测试
+(check-true (symbol? 'complex_name))
+(check-true (symbol? 'symbol_with_underscore))
+(check-true (symbol? 'symbol-with-dash))
+
+;; 非符号类型测试
+(check-false (symbol? 123))
+(check-false (symbol? 123.456))
+(check-false (symbol? #\a))
+(check-false (symbol? '()))
+(check-false (symbol? (list 'a 'b 'c)))
+(check-false (symbol? (vector 'a 'b 'c)))
+
+;; 字符串转换测试
+(check-true (symbol? (string->symbol "test")))
+(check-true (symbol? (string->symbol "complex-symbol-with-numbers")))
+
+(check-catch 'wrong-number-of-args (symbol=? 'a))
+(check-catch 'wrong-number-of-args (symbol=? 1))
+
+(check-true (symbol=? 'a 'a))
+(check-true (symbol=? 'foo 'foo))
+(check-false (symbol=? 'a 'b))
+(check-false (symbol=? 'foo 'bar))
+
+(check-true (symbol=? 'bar 'bar 'bar))
+
+(check-true (symbol=? (string->symbol "foo") (string->symbol "foo")))
+(check-false (symbol=? (string->symbol "foo") (string->symbol "bar")))
+
+(check-false (symbol=? 1 1))
+(check-false (symbol=? 'a 1))
+(check-false (symbol=? (string->symbol "foo") 1))
+
+(check-false (symbol=? 'a 'b '()))
+
+(check (symbol->string `MathAgape) => "MathAgape")
+(check (symbol->string 'goldfish-scheme) => "goldfish-scheme")
+
+(check (symbol->string (string->symbol "MathApage")) => "MathApage")
+(check (symbol->string (string->symbol "Hello World")) => "Hello World")
+
+(check (string->symbol "MathAgape") => `MathAgape)
+(check-false (equal? (string->symbol "123") '123))
+(check (string->symbol "+") => '+)
+
+(check (string->symbol (symbol->string `MathAgape)) => `MathAgape)
+
 
 (check (string? "MathAgape") => #t)
 (check (string? "") => #t)
