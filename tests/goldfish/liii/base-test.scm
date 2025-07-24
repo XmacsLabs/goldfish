a6ddbb32c7af06e1e38b001dfa44681a32ca3e38
沈达 <da@liii.pro>
2025-07-24 13:09:46 +0000

    !630 [201_7] 完善symbol->string测试用例和文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -2105,11 +2105,64 @@ symbol : symbol?
 string?
 符号对应的字符串表示
 
+错误
+----
+wrong-type-arg
+如果参数不是符号类型，抛出错误。
+
+说明
+----
+symbol->string将符号的标识符转换为等效的字符串表示。
+注意区分大小写：符号'abc和'ABC会转换为"abc"和"ABC"的不同字符串。
+
 |#
 
-
-(check (symbol->string (string->symbol "MathApage")) => "MathApage")
+;; 基本测试
+(check (symbol->string 'MathAgape) => "MathAgape")
+(check (symbol->string 'goldfish-scheme) => "goldfish-scheme")
 (check (symbol->string (string->symbol "Hello World")) => "Hello World")
+
+;; 特殊符号测试
+(check (symbol->string '+) => "+")
+(check (symbol->string '-) => "-")
+(check (symbol->string '*) => "*")
+(check (symbol->string '/) => "/")
+(check (symbol->string '=>) => "=>")
+(check (symbol->string '<=) => "<=")
+
+;; 大小写敏感测试
+(check (symbol->string 'ABC) => "ABC")
+(check (symbol->string 'abc) => "abc")
+(check (symbol->string 'LispCase) => "LispCase")
+(check (symbol->string 'camelCase) => "camelCase")
+
+;; 边界测试
+(check (symbol->string 'a) => "a")
+(check (symbol->string 'x) => "x")
+(check (symbol->string 'empty) => "empty")
+
+;; 数字和特殊字符测试
+(check (symbol->string (string->symbol "123")) => "123")
+(check (symbol->string (string->symbol "123abc")) => "123abc")
+(check (symbol->string (string->symbol "symbol_with_underscore")) => "symbol_with_underscore")
+(check (symbol->string (string->symbol "symbol-with-dash")) => "symbol-with-dash")
+(check (symbol->string (string->symbol "sym$bol")) => "sym$bol")
+
+;; 错误测试
+(check-catch 'wrong-type-arg (symbol->string 123))
+(check-catch 'wrong-type-arg (symbol->string "symbol"))
+(check-catch 'wrong-type-arg (symbol->string #f))
+(check-catch 'wrong-type-arg (symbol->string '()))
+(check-catch 'wrong-number-of-args (symbol->string 'a 'b))
+(check-catch 'wrong-number-of-args (symbol->string))
+
+;; 往返转换测试
+(let ((test-symbols '(hello world scheme-prog example complex-identifier my-symbol)))
+  (for-each
+    (lambda (sym)
+      (let ((str (symbol->string sym)))
+        (check (string->symbol str) => sym)))
+    test-symbols))
 
 (check (string->symbol "MathAgape") => `MathAgape)
 (check-false (equal? (string->symbol "123") '123))
