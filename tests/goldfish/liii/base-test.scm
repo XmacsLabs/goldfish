017cefcad80523504934ff3c2b4e72f9471260f4
沈达 <da@liii.pro>
2025-07-24 13:03:53 +0000

    !629 [201_7] 完成symbol=?的测试和文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -2038,6 +2038,32 @@ boolean?
 (check-true (symbol? (string->symbol "test")))
 (check-true (symbol? (string->symbol "complex-symbol-with-numbers")))
 
+#|
+symbol=?
+判断给定符号是否相等
+
+语法
+----
+(symbol=? symbol1 symbol2 ...)
+
+参数
+----
+symbol1, symbol2, ... : symbol?
+一个或多个符号参数
+
+返回值
+-----
+boolean?
+如果所有符号相等则返回#t，否则返回#f
+
+说明
+----
+符号比较是基于符号的标识符名称进行的。
+R7RS中规定symbol=?需要至少两个参数。
+
+|#
+
+;; 基本测试
 (check-catch 'wrong-number-of-args (symbol=? 'a))
 (check-catch 'wrong-number-of-args (symbol=? 1))
 
@@ -2046,19 +2072,41 @@ boolean?
 (check-false (symbol=? 'a 'b))
 (check-false (symbol=? 'foo 'bar))
 
+;; 多参数测试
 (check-true (symbol=? 'bar 'bar 'bar))
+(check-true (symbol=? 'x 'x 'x 'x))
+(check-false (symbol=? 'a 'a 'b))
 
-(check-true (symbol=? (string->symbol "foo") (string->symbol "foo")))
-(check-false (symbol=? (string->symbol "foo") (string->symbol "bar")))
+;; 边界测试
+(check-true (symbol=? 'a (string->symbol "a")))
+(check-false (symbol=? 'a (string->symbol "A")))
 
+;; 类型错误测试
 (check-false (symbol=? 1 1))
 (check-false (symbol=? 'a 1))
 (check-false (symbol=? (string->symbol "foo") 1))
-
 (check-false (symbol=? 'a 'b '()))
 
-(check (symbol->string `MathAgape) => "MathAgape")
-(check (symbol->string 'goldfish-scheme) => "goldfish-scheme")
+#|
+symbol->string
+将符号转换为字符串形式
+
+语法
+----
+(symbol->string symbol)
+
+参数
+----
+symbol : symbol?
+要转换的符号
+
+返回值
+-----
+string?
+符号对应的字符串表示
+
+|#
+
 
 (check (symbol->string (string->symbol "MathApage")) => "MathApage")
 (check (symbol->string (string->symbol "Hello World")) => "Hello World")
