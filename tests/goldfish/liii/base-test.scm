d0b614f4a3a7b7292a21a3c0761ae323b79ff2da
fzw-codelearner <3498363975@qq.com>
2025-07-25 02:47:47 +0000

    !633 [201_3] 完善quotient文档和测试

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -1525,15 +1525,73 @@ wrong-type-arg
 (check (floor-quotient 0 2) => 0)
 (check (floor-quotient 0 -2) => 0)
 
+#|
+quotient
+用于计算两个数的精确除法商（向零取整）。
+
+语法
+----
+(quotient dividend divisor)
+
+参数
+----
+dividend : real? - 被除数
+divisor : real? - 除数，不能为零
+
+返回值
+------
+integer?
+返回一个整数，表示向零方向取整的商。
+
+与floor-quotient的区别
+-------------
+quotient与floor-quotient的主要区别在于对负数除法的处理：
+- quotient：向零取整（截断除法），如(quotient -11 2) => -5
+- floor-quotient：向负无穷取整，如(floor-quotient -11 2) => -6
+
+错误
+----
+division-by-zero
+当除数为零时抛出错误。
+wrong-type-arg
+当参数不是数字时抛出错误。
+|#
 
 (check (quotient 11 2) => 5)
 (check (quotient 11 -2) => -5)
 (check (quotient -11 2) => -5)
 (check (quotient -11 -2) => 5)
 
+(check (quotient 10 3) => 3)
+(check (quotient 10 -3) => -3)
+(check (quotient -10 3) => -3)
+(check (quotient -10 -3) => 3)
+
+(check (quotient 0 5) => 0)
+(check (quotient 0 -5) => 0)
+(check (quotient 15 5) => 3)
+(check (quotient -15 5) => -3)
+
+(check (quotient 7 7) => 1)
+(check (quotient 100 10) => 10)
+(check (quotient 1 1) => 1)
+(check (quotient -1 1) => -1)
+
+(check (quotient 17 5) => 3)
+(check (quotient -17 5) => -3)
+(check (quotient 17 -5) => -3)
+(check (quotient -17 -5) => 3)
+
 (check-catch 'division-by-zero (quotient 11 0))
 (check-catch 'division-by-zero (quotient 0 0))
+(check (quotient 10.5 3.0) => 3)
+(check (quotient 10.5 -3.0) => -3)
+(check (quotient -10.5 3.0) => -3)
+(check (quotient -10.5 -3.0) => 3)
 (check-catch 'wrong-type-arg (quotient 1+i 2))
+(check-catch 'wrong-type-arg (quotient 'hello 2))
+(check-catch 'wrong-number-of-args (quotient 10))
+(check-catch 'wrong-number-of-args (quotient 5 3 2))
 
 (check (modulo 13 4) => 1)    
 (check (modulo -13 4) => 3)    
