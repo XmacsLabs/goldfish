7acd765e1d3f72cd53fcbaecd1654acf26b0aa0e
fzw-codelearner <3498363975@qq.com>
2025-07-28 06:03:52 +0000

    !647 [201_3] 添加boolean=?和char=?的文档和测试

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -2035,18 +2035,140 @@ wrong-number-of-args
 (check-catch 'wrong-number-of-args (number->string))
 (check-catch 'wrong-number-of-args (number->string 123 2 3))
 
-(check-true (boolean=? #t #t))
-(check-true (boolean=? #f #f))
-(check-true (boolean=? #t #t #t))
-(check-false (boolean=? #t #f))
-(check-false (boolean=? #f #t))
+#|
+boolean=?
+比较两个或多个布尔值是否相等。
+
+语法
+----
+(boolean=? bool1 bool2 . more-bools)
+
+参数
+----
+bool1, bool2, ... : boolean?
+布尔值，可以是 #t (真) 或 #f (假)。
+
+返回值
+------
+boolean?
+如果所有给定的布尔值都相等，则返回 #t (真)，否则返回 #f (假)。
+
+说明
+----
+1. 至少需要两个参数
+2. 所有参数必须都是布尔值 (#t 或 #f)
+3. 当所有布尔值相同时返回 #t，否则返回 #f
+4. 支持比较两个或多个布尔值
+
+错误处理
+--------
+wrong-number-of-args
+当参数数量少于2个时抛出错误。
+|#
+
+;; boolean=? 基本测试
+(check (boolean=? #t #t) => #t)
+(check (boolean=? #f #f) => #t)
+(check (boolean=? #t #f) => #f)
+(check (boolean=? #f #t) => #f)
+(check (boolean=? 1 #t) => #f)
+
+;; 多参数测试
+(check (boolean=? #t #t #t) => #t)
+(check (boolean=? #f #f #f) => #t)
+(check (boolean=? #t #t #f) => #f)
+(check (boolean=? #t #f #t) => #f)
+(check (boolean=? #f #t #t) => #f)
+
+;; 边界测试
+(check (boolean=? #t #t #t #t #t) => #t)
+(check (boolean=? #f #f #f #f #f) => #t)
+(check (boolean=? #t #t #f #t #t) => #f)
+
+;; 错误处理测试
+(check-catch 'wrong-number-of-args (boolean=?))
+(check-catch 'wrong-number-of-args (boolean=? #t))
+
 
 (check (char? #\A) => #t)
 (check (char? 1) => #f)
 
+#|
+char=?
+比较两个或多个字符是否相等。
+
+语法
+----
+(char=? char1 char2 . more-chars)
+
+参数
+----
+char1, char2, ... : char?
+字符值。
+
+返回值
+------
+boolean?
+如果所有给定的字符都相等，则返回 #t (真)，否则返回 #f (假)。
+
+说明
+----
+1. 至少需要两个参数
+2. 所有参数必须都是字符
+3. 当所有字符相等时返回 #t，否则返回 #f
+4. 支持比较两个或多个字符
+5. 区分大小写
+
+错误处理
+--------
+wrong-type-arg
+当参数不是字符时抛出错误。
+wrong-number-of-args
+当参数数量少于2个时抛出错误。
+|#
+
+;; char=? 基本测试
 (check (char=? #\A #\A) => #t)
-(check (char=? #\A #\A #\A) => #t)
+(check (char=? #\a #\a) => #t)
 (check (char=? #\A #\a) => #f)
+(check (char=? #\a #\A) => #f)
+(check (char=? #\0 #\0) => #t)
+(check (char=? #\9 #\9) => #t)
+(check (char=? #\0 #\9) => #f)
+
+;; 特殊字符测试
+(check (char=? #\space #\space) => #t)
+(check (char=? #\newline #\newline) => #t)
+(check (char=? #\tab #\tab) => #t)
+(check (char=? #\space #\newline) => #f)
+
+;; 多参数测试
+(check (char=? #\A #\A #\A) => #t)
+(check (char=? #\a #\a #\a) => #t)
+(check (char=? #\A #\A #\a) => #f)
+(check (char=? #\a #\b #\c) => #f)
+
+;; 边界测试
+(check (char=? #\0 #\0 #\0 #\0 #\0) => #t)
+(check (char=? #\A #\A #\A #\A #\a) => #f)
+(check (char=? #\z #\z #\z) => #t)
+(check (char=? #\! #\! #\!) => #t)
+
+;; 数字字符测试
+(check (char=? #\1 #\1) => #t)
+(check (char=? #\1 #\! ) => #f)
+
+;; 大小写混合测试
+(check (char=? #\a #\b #\c #\d) => #f)
+(check (char=? #\A #\B #\C) => #f)
+
+;; 错误处理测试
+(check-catch 'wrong-type-arg (char=? 1 #\A))
+(check-catch 'wrong-type-arg (char=? #\A 'symbol))
+(check-catch 'wrong-type-arg (char=? 123 #\a))
+(check-catch 'wrong-number-of-args (char=?))
+(check-catch 'wrong-number-of-args (char=? #\A))
+
 
 (check (char->integer #\A) => 65)
 (check (char->integer #\a) => 97)
