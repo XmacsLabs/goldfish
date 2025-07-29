4b57bc58d98b239949547330ed9a70029c35a80f
fzw-codelearner <3498363975@qq.com>
2025-07-29 08:03:48 +0000

    !655 [201_3] 添加exp和log函数文档及其测试

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -1908,6 +1908,129 @@ wrong-type-arg
 (check (denominator (inexact->exact 5.5)) => 2)
 (check (denominator (inexact->exact 0.25)) => 4)
 
+#|
+exp
+计算指数函数 e^n。
+
+语法
+----
+(exp n)
+
+参数
+----
+n : number?
+可选的数值参数，指数值。
+
+返回值
+------
+number?
+e的n次幂值。
+
+说明
+----
+1. 计算自然指数函数
+2. e ≈ 2.718281828459045
+3. 支持整数、有理数、浮点数、复数等各种数值类型
+
+错误处理
+--------
+wrong-type-arg
+当参数不是数时抛出错误。
+wrong-number-of-args
+当参数数量不为1时抛出错误。
+|#
+
+;; exp 基本测试
+(check (exp 0) => 1)
+(check (exp 1) => 2.718281828459045)
+(check (exp -1) => 0.36787944117144233)
+(check (exp 2) => 7.38905609893065)
+
+;; exp 边界测试
+(check (exp 10) => 22026.465794806718)
+(check (exp -10) => 4.5399929762484854e-05)
+(check (exp 0.5) => 1.6487212707001282)
+(check (exp -0.5) => 0.6065306597126334)
+
+;; 错误处理测试
+(check (exp 1+2i) => -1.1312043837568135+2.4717266720048188i)
+(check-catch 'wrong-type-arg (exp "hello"))
+(check-catch 'wrong-number-of-args (exp))
+
+#|
+log
+计算对数函数。单个参数时计算自然对数(log base e)，两个参数时计算以第二个参数为底的对数。
+
+语法
+----
+(log z [base])
+
+参数
+----
+z : number?
+必须为数，计算对数值
+
+base : number? 可选
+必须为数，表示对数底
+
+返回值
+------
+number?
+对应的对数值
+
+说明
+----
+1. 单个参数：计算自然对数ln(z) = log_e(z)
+2. 两个参数：计算log_base(z) = log(z)/log(base)
+3. 支持各种数值类型
+4. 注意参数必须为正数且不等于1
+
+错误处理
+--------
+out-of-range
+当z <= 0或base <= 0时抛出错误。
+wrong-type-arg
+当参数类型错误时抛出错误。
+wrong-number-of-args
+当参数数量不为1或2个时抛出错误。
+|#
+
+;; log 基本自然对数测试
+(check (log 1) => 0.0)
+(check (log (exp 1)) => 1.0)
+(check (log 2) => 0.6931471805599453)
+
+;; log 双参数对数测试
+(check (log 100 10) => 2)
+(check (log 8 2) => 3)
+(check (log 16 2) => 4)
+
+;; log 通用对数测试
+(check (log 10 10) => 1)
+(check (log 100 10) => 2)
+(check (log 1 10) => 0)
+
+;; log 有理数测试
+(check (log 2 4) => 1/2)
+(check (log 1/2 2) => -1.0)
+(check (log 9 3) => 2)
+
+;; log 浮点数对数测试
+(check (log 2.718281828459045) => 1.0)
+(check (log 0.1 10) =>  -0.9999999999999998) ;返回值是个不精确数
+
+;; 相互验证测试
+(check (log (exp 3)) => 3.0)
+(check (exp (log 5)) => 4.999999999999999)
+
+;; 错误处理测试
+(check (log 0) => -inf.0+3.141592653589793i) ; log(0) = -∞ + πi
+(check (log -1) => 0+3.141592653589793i) ; log(-1) = πi
+(check (log 3 1) => +inf.0) ; log(3, 1) = 0
+(check-catch 'out-of-range (log 10 0))
+(check-catch 'wrong-type-arg (log "a"))
+(check-catch 'wrong-number-of-args (log))
+(check-catch 'wrong-number-of-args (log 12 4 5))
 
 #|
 square
