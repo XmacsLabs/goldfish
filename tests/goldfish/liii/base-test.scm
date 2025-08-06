a1f455e8c10384e080a87f536d19677b2d932171
annaeina <2846698728@qq.com>
2025-08-06 10:19:15 +0000

    !708 [201_11] 生成 R7RS 中 string=?，string-set! 函数的文档和测试用例

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -5636,6 +5636,236 @@ wrong-type-arg
 
 
 #|
+string-set!
+修改字符串中指定位置的字符，返回修改后的字符串。在R7RS标准中，string-set!是一个立即执行的变异操作，不会创建新的字符串对象。
+
+语法
+----
+(string-set! string k char)
+
+参数
+----
+string : string? 
+要修改的原始字符串。必须是非常量字符串。
+
+k : exact?
+必须是非负的精确整数，表示要修改的字符索引位置。必须小于字符串长度。
+
+char : char?
+新的字符值，用于替换位置k处的原始字符。
+
+返回值
+------
+unspecified
+按照R7RS规范，返回未指定的值。
+
+说明
+----
+1. 这是一个变异操作，会直接修改原始字符串对象的内容
+2. 索引k是从0开始计算的
+3. 可以用来修改任何位置的合法字符，但不能用于扩展字符串长度
+4. 修改后的字符串与新字符串不同的引用指向相同的内存内容
+5. 参数必须是变量引用或动态创建的字符串，不能是字符串常量
+
+错误处理
+--------
+out-of-range
+当索引k为负数或大于等于字符串长度时抛出错误。
+
+wrong-type-arg
+当string不是字符串、k不是精确整数、char不是字符时抛出错误。
+|#
+
+;; string-set! 基础测试
+(let1 str (string-copy "hello")
+  (string-set! str 1 #\A)
+  (check str => "hAllo"))
+
+(let1 str (string-copy "abc")
+  (string-set! str 0 #\X)
+  (string-set! str 2 #\Z)
+  (check str => "XbZ"))
+
+;; 修改不同位置测试
+(let1 str (string-copy "123456")
+  (string-set! str 0 #\0)
+  (string-set! str 5 #\9)
+  (check str => "023459"))
+
+;; 边界位置测试
+(let1 str (string-copy "a") 
+  (string-set! str 0 #\A)
+  (check str => "A"))
+
+(let1 str (string-copy "xyz")
+  (string-set! str 0 #\1)
+  (string-set! str 1 #\2) 
+  (string-set! str 2 #\3)
+  (check str => "123"))
+
+;; 特殊字符测试
+(let1 str (string-copy "hello world")
+  (string-set! str 5 #\-)
+  (check str => "hello-world"))
+
+(let1 str (string-copy "Test!")
+  (string-set! str 4 #\?)
+  (check str => "Test?"))
+
+;; 数字字符串测试
+(let1 str (string-copy "00000")
+  (string-set! str 2 #\1)
+  (check str => "00100"))
+
+;; 连续多次修改
+(let1 str (string-copy "original")
+  (string-set! str 0 #\O)
+  (string-set! str 1 #\R)
+  (string-set! str 2 #\I)
+  (string-set! str 3 #\G)
+  (string-set! str 4 #\I)
+  (string-set! str 5 #\N)
+  (string-set! str 6 #\A)
+  (string-set! str 7 #\L)
+  (check str => "ORIGINAL"))
+
+;; 测试索引在有效范围内
+(let1 str (string-copy "test")
+  (string-set! str 0 #\T)
+  (string-set! str 1 #\E)
+  (string-set! str 2 #\S)
+  (string-set! str 3 #\T)
+  (check str => "TEST"))
+
+;; 错误处理测试
+;; 索引越界测试
+(let1 str (string-copy "abc")
+  (check-catch 'out-of-range (string-set! str -1 #\x))
+  (check-catch 'out-of-range (string-set! str 3 #\x)))
+
+(let1 str (string-copy "")
+  (check-catch 'out-of-range (string-set! str 0 #\x)))
+
+;; 类型错误测试
+(check-catch 'wrong-type-arg (string-set! 123 0 #\A))
+(check-catch 'wrong-type-arg (string-set! "hello" 0.5 #\A))
+(check-catch 'wrong-type-arg (string-set! "hello" 0 123))
+(check-catch 'wrong-type-arg (string-set! "hello" 1 "A"))
+
+;; 参数数量错误测试
+(check-catch 'wrong-number-of-args (string-set!))
+(check-catch 'wrong-number-of-args (string-set! "hello"))
+(check-catch 'wrong-number-of-args (string-set! "hello" 1))
+(check-catch 'wrong-number-of-args (string-set! "hello" 1 #\a #\b))
+
+;; 变量引用一致性测试
+(let1 str1 (string-copy "hello")
+  (let1 str2 str1
+    (string-set! str1 1 #\E)
+    (check str1 => "hEllo")
+    (check str2 => "hEllo")))
+
+;; 与string-ref结合使用测试
+(let1 str (string-copy "test")
+  (check (string-ref str 0) => #\t)
+  (string-set! str 0 #\T)
+  (check (string-ref str 0) => #\T)
+  (check str => "Test"))
+
+;; 复杂字符串修改场景测试
+(let1 str (string-copy "programming")
+  (string-set! str 0 #\P)
+  (string-set! str 8 #\N)
+  (string-set! str 10 #\G)
+  (check (string-ref str 0) => #\P)
+  (check (string-ref str 8) => #\N)
+  (check (string-ref str 10) => #\G))
+
+#|
+string=?
+比较两个字符串是否相等。
+
+语法
+----
+(string=? string1 string2 ...)
+
+参数
+----
+string1, string2, ... : string?
+至少两个字符串参数。
+
+返回值
+------
+boolean?
+如果所有字符串都相等则返回 #t，否则返回 #f。比较区分大小写。
+
+说明
+----
+1. 至少需要两个参数
+2. 所有参数必须是字符串类型
+3. 字符串比较区分大小写
+4. 当所有字符串内容完全相同返回 #t
+5. 返回布尔值结果
+
+边界情况
+--------
+- 区分大小写：大写和小写字母被认为不同
+- 空字符串比较：空字符串只与空字符串相等
+- 特殊字符：所有字符都需要完全相同，包括空格、制表符等
+
+错误处理
+--------
+wrong-number-of-args
+当参数数量少于2个时抛出错误。
+|#
+
+;; string=? 基本测试
+(check-true (string=? "hello" "hello"))
+(check-true (string=? "" ""))
+(check-true (string=? "a" "a"))
+
+;; string=? 区分大小写测试
+(check-false (string=? "Hello" "hello"))
+(check-false (string=? "HELLO" "hello"))
+(check-false (string=? "abc" "ABC"))
+
+;; string=? 不同内容测试
+(check-false (string=? "hello" "world"))
+(check-false (string=? "abc" "def"))
+(check-false (string=? "short" "longer"))
+
+;; string=? 各种边界情况
+(check-true (string=? "123" "123"))
+(check-true (string=? "!@#$%" "!@#$%"))
+(check-true (string=? "空格 测试" "空格 测试"))
+(check-false (string=? "abc" "abcd"))
+(check-false (string=? "abcd" "abc"))
+
+;; string=? 特殊字符测试
+(check-true (string=? "\n\t" "\n\t"))
+(check-true (string=? "测试文本" "测试文本"))
+(check-false (string=? "测试" "测试文本"))
+(check-false (string=? "测试文本" "测试"))
+
+;; string=? 空字符串测试
+(check-true (string=? "" "" "") => #t)
+(check-true (string=? "a" "a" "a"))
+
+;; string=? 多参数测试
+(check-true (string=? "same" "same" "same"))
+(check-false (string=? "same" "diff" "same"))
+(check-false (string=? "one" "two" "three"))
+
+;; string=? 二进制和Unicode字符串
+(check-true (string=? "Hello, 世界!" "Hello, 世界!"))
+(check-false (string=? "Hello, 世界!" "Hello, 世界! "))
+
+;; 错误处理测试
+(check-catch 'wrong-number-of-args (string=?))
+(check-catch 'wrong-number-of-args (string=? "hello"))
+
+
+#|
 bytevector
 返回一个新分配的字节向量，其元素包含传递给过程的所有参数。每个参数都必须是一个介于0到255之间的整数，表示字节向量中的一个字节。如果没有提供任何参数，将创建一个空的字节向量。
 
