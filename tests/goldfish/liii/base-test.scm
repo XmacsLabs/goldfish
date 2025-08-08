0ded7628a4c0b5cc0548c1e4fc1e7cb001a82e08
Yuki <yifan@liii.pro>
2025-08-08 16:10:51 +0800

    zero?的测试和文档

--- tests/goldfish/liii/base-test.scm
+++ tests/goldfish/liii/base-test.scm
@@ -896,6 +896,52 @@ wrong-type-arg
 (check-catch 'wrong-type-arg (zero? #f))
 
 #|
+zero?
+判断一个数值是否为零。
+
+语法
+----
+(zero? obj)
+
+参数
+----
+obj : number?
+任意数值类型。
+
+返回值
+-----
+boolean?
+如果 obj 是数值，当其为零时返回 #t，否则返回 #f。
+
+错误
+----
+wrong-type-arg
+如果参数不是数值类型
+
+|#
+
+(check-true (zero? 0))
+(check-true (zero? 0.0))
+(check-true (zero? 0+0i))
+(check-true (zero? 0/10))
+
+(check-false (zero? 1))
+(check-false (zero? 1.0))
+(check-false (zero? -1))
+(check-false (zero? -1.0))
+(check-false (zero? +inf.0))
+(check-false (zero? -inf.0))
+(check-false (zero? +nan.0))
+(check-false (zero? 1+i))
+
+(check-catch 'wrong-type-arg (zero? #\A))
+(check-catch 'wrong-type-arg (zero? #t))
+(check-catch 'wrong-type-arg (zero? #f))
+(check-catch 'wrong-type-arg (zero? "not-a-number"))
+(check-catch 'wrong-type-arg (zero? 'symbol))
+(check-catch 'wrong-type-arg (zero? '(1 2 3)))
+
+#|
 positive?
 判断一个对象是否是正数。
 
