7ee3ddeb9960c48cb43bbf66de91b0934a7adab8
沈达 <da@liii.pro>
2025-09-21 07:41:05 +0000

    !852 [201_13] 补充(liii error)这个依赖，LSP可以支持跳转

--- goldfish/liii/rich-list.scm
+++ goldfish/liii/rich-list.scm
@@ -1,3 +1,19 @@
+;
+; Copyright (C) 2025 The Goldfish Scheme Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
 (define-library (liii rich-list)
   (import (liii list)
           (liii oop)
@@ -5,7 +21,8 @@
           (liii hash-table)
           (liii string)
           (liii option)
-          (srfi srfi-8))
+          (srfi srfi-8)
+          (liii error))
   (export rich-list)
   (begin
 
