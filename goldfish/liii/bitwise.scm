c569b95ad58c70f847c77a562bdc7737c49c7f5b
沈达 <da@liii.pro>
2025-09-21 14:56:52 +0000

    !859 [200_15] add more file to the formatted list

--- goldfish/liii/bitwise.scm
+++ goldfish/liii/bitwise.scm
@@ -15,23 +15,15 @@
 ;
 
 (define-library (liii bitwise)
-  (import (srfi srfi-151)
-          (liii error))
-  (export
-    ; from (srfi srfi-151)
-    bitwise-not bitwise-and bitwise-ior bitwise-xor bitwise-eqv  bitwise-or bitwise-nor bitwise-nand
-    bit-count bitwise-orc1 bitwise-orc2 bitwise-andc1 bitwise-andc2
-    arithmetic-shift integer-length bitwise-if
-    bit-set? copy-bit bit-swap any-bit-set? every-bit-set? first-set-bit
-    bit-field bit-field-any? bit-field-every? bit-field-clear bit-field-set
-    ; S7 built-in
-    lognot logand logior logxor
-    ash
-    )
+  (import (srfi srfi-151) (liii error))
+  ; S7 built-in
+  (export lognot logand logior logxor ash)
+  ; from (srfi srfi-151)
+  (export bitwise-not bitwise-and bitwise-ior bitwise-xor bitwise-eqv bitwise-or bitwise-nor
+          bitwise-nand bit-count bitwise-orc1 bitwise-orc2 bitwise-andc1 bitwise-andc2
+          arithmetic-shift integer-length bitwise-if bit-set? copy-bit bit-swap any-bit-set?
+          every-bit-set? first-set-bit bit-field bit-field-any? bit-field-every? bit-field-clear
+          bit-field-set)
   (begin
-
-    (define bitwise-or bitwise-ior)
-
-    ) ; end of begin
-  ) ; end of library
+    (define bitwise-or bitwise-ior)))
 
