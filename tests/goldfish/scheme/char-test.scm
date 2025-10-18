7226e5d2a546a859188230e1f3e771cef24b149e
沈达 <da@liii.pro>
2025-10-18 17:20:03 +0000

    !877 [201_10] char-ci<? 和 char-ci>? 的测试用例和文档的迁移

--- tests/goldfish/scheme/char-test.scm
+++ tests/goldfish/scheme/char-test.scm
@@ -765,5 +765,213 @@ boolean?
 (check-catch 'wrong-number-of-args (char-ci=?))
 (check-catch 'wrong-number-of-args (char-ci=? #\A))
 
+#|
+char-ci<?
+按大小写不敏感的方式比较字符是否小于。
+
+函数签名
+-----
+(char-ci<? char1 char2 char3 ...) → boolean?
+
+参数
+----
+char1, char2, char3, ... : character
+要比较的字符
+
+返回值
+----
+boolean?
+如果字符序列在大小写不敏感的情况下严格升序，则返回 #t，否则返回 #f
+
+描述
+----
+`char-ci<?` 用于按大小写不敏感的方式比较字符是否小于。该函数忽略字符的大小写差异，
+将大写字母和小写字母视为相等，然后比较它们的顺序。
+
+行为特征
+------
+- 对于相同字母的大小写不同形式（如 #\a 和 #\A），返回 #f（相等）
+- 对于不同的字符，按大小写不敏感的方式比较顺序
+- 支持多个参数，只有当所有字符在大小写不敏感的情况下严格升序时才返回 #t
+- 遵循 R7RS 标准规范
+
+错误处理
+------
+- 所有参数必须是字符类型，否则会抛出 `type-error` 异常
+- 至少需要两个参数，否则会抛出 `wrong-number-of-args` 异常
+
+实现说明
+------
+- 函数在 R7RS 标准库中定义，在 (scheme char) 库中提供
+- 使用底层 S7 的 char-ci<? 函数实现核心功能
+- 添加了参数类型检查，确保所有参数都是字符类型
+
+相关函数
+--------
+- `char<?` : 按大小写敏感的方式比较字符是否小于
+- `char-ci=?` : 按大小写不敏感的方式比较字符是否相等
+- `char-ci>?` : 按大小写不敏感的方式比较字符是否大于
+- `char-ci<=?` : 按大小写不敏感的方式比较字符是否小于等于
+- `char-ci>=?` : 按大小写不敏感的方式比较字符是否大于等于
+|#
+
+;; char-ci<? 基本功能测试
+(check (char-ci<? #\a #\B) => #t)
+(check (char-ci<? #\A #\b) => #t)
+(check (char-ci<? #\A #\a) => #f)  ; 等大写和小写不严格升序
+(check (char-ci<? #\a #\A) => #f)
+(check (char-ci<? #\Z #\a) => #f)  ; A-Z在a-z之前大写
+(check (char-ci<? #\z #\A) => #f)
+
+;; 大小写一致测试
+(check (char-ci<? #\a #\b) => #t)
+(check (char-ci<? #\A #\B) => #t)
+(check (char-ci<? #\B #\a) => #f)  ; B > a小写
+(check (char-ci<? #\z #\A) => #f)
+
+;; 相等字符测试
+(check (char-ci<? #\a #\A) => #f)
+(check (char-ci<? #\A #\A) => #f)
+(check (char-ci<? #\a #\a) => #f)
+
+;; 特殊字符测试
+(check (char-ci<? #\space #\newline) => #f)
+(check (char-ci<? #\tab #\space) => #t)
+(check (char-ci<? #\! #\@) => #t)
+(check (char-ci<? #\! #\!) => #f)
+
+;; 多参数测试
+(check (char-ci<? #\a #\B #\c #\D) => #t)
+(check (char-ci<? #\A #\b #\C #\d) => #t)
+(check (char-ci<? #\a #\A #\b #\B) => #f)  ; 等大写和小写
+(check (char-ci<? #\a #\z #\A #\B) => #f)
+
+;; 数字字符测试
+(check (char-ci<? #\0 #\1) => #t)
+(check (char-ci<? #\9 #\0) => #f)
+(check (char-ci<? #\1 #\A) => #t)
+(check (char-ci<? #\9 #\a) => #t)
+
+;; 字母范围测试
+(check (char-ci<? #\a #\z) => #t)
+(check (char-ci<? #\A #\Z) => #t)
+(check (char-ci<? #\z #\a) => #f)
+(check (char-ci<? #\Z #\a) => #f)
+
+;; 特殊字符边界测试
+(check (char-ci<? #\0 #\! ) => #f)
+(check (char-ci<? #\space #\!) => #t)
+(check (char-ci<? #\tab #\newline) => #t)
+
+;; 错误处理测试 - 更新为 type-error
+(check-catch 'type-error (char-ci<? 1 #\A))
+(check-catch 'type-error (char-ci<? #\A 'symbol))
+(check-catch 'wrong-number-of-args (char-ci<?))
+(check-catch 'wrong-number-of-args (char-ci<? #\A))
+
+#|
+char-ci>?
+按大小写不敏感的方式比较字符是否大于。
+
+函数签名
+-----
+(char-ci>? char1 char2 char3 ...) → boolean?
+
+参数
+----
+char1, char2, char3, ... : character
+要比较的字符
+
+返回值
+----
+boolean?
+如果字符序列在大小写不敏感的情况下严格降序，则返回 #t，否则返回 #f
+
+描述
+----
+`char-ci>?` 用于按大小写不敏感的方式比较字符是否大于。该函数忽略字符的大小写差异，
+将大写字母和小写字母视为相等，然后比较它们的顺序。
+
+行为特征
+------
+- 对于相同字母的大小写不同形式（如 #\a 和 #\A），返回 #f（相等）
+- 对于不同的字符，按大小写不敏感的方式比较顺序
+- 支持多个参数，只有当所有字符在大小写不敏感的情况下严格降序时才返回 #t
+- 遵循 R7RS 标准规范
+
+错误处理
+------
+- 所有参数必须是字符类型，否则会抛出 `type-error` 异常
+- 至少需要两个参数，否则会抛出 `wrong-number-of-args` 异常
+
+实现说明
+------
+- 函数在 R7RS 标准库中定义，在 (scheme char) 库中提供
+- 使用底层 S7 的 char-ci>? 函数实现核心功能
+- 添加了参数类型检查，确保所有参数都是字符类型
+
+相关函数
+--------
+- `char>?` : 按大小写敏感的方式比较字符是否大于
+- `char-ci=?` : 按大小写不敏感的方式比较字符是否相等
+- `char-ci<?` : 按大小写不敏感的方式比较字符是否小于
+- `char-ci<=?` : 按大小写不敏感的方式比较字符是否小于等于
+- `char-ci>=?' : 按大小写不敏感的方式比较字符是否大于等于
+|#
+
+;; char-ci>? 基本功能测试
+(check (char-ci>? #\B #\a) => #t)
+(check (char-ci>? #\b #\A) => #t)
+(check (char-ci>? #\a #\A) => #f)  ; 等大写和小写不严格降序
+(check (char-ci>? #\A #\a) => #f)
+(check (char-ci>? #\a #\z) => #f)
+(check (char-ci>? #\Z #\a) => #t)  ; Z > a
+
+;; 大小写一致测试
+(check (char-ci>? #\b #\a) => #t)
+(check (char-ci>?  #\B #\A) => #t)
+(check (char-ci>? #\a #\B) => #f)  ; a < B
+(check (char-ci>? #\z #\A) => #t)
+
+;; 相等字符测试（不严格降序）
+(check (char-ci>? #\a #\A) => #f)
+(check (char-ci>? #\A #\A) => #f)
+(check (char-ci>? #\a #\a) => #f)
+
+;; 特殊字符测试
+(check (char-ci>? #\newline #\space) => #f)
+(check (char-ci>? #\space #\tab) => #t)
+(check (char-ci>? #\@ #\!) => #t)
+(check (char-ci>? #\! #\!) => #f)
+
+;; 多参数不敏感降序测试
+(check (char-ci>? #\D #\c #\B #\a) => #t)
+(check (char-ci>? #\d #\C #\b #\A) => #t)
+(check (char-ci>? #\z #\z #\a) => #f)  ; 等大写和小写
+(check (char-ci>? #\b #\a #\C) => #f)
+
+;; 数字字符测试（大小写不影响）
+(check (char-ci>? #\9 #\0) => #t)
+(check (char-ci>?  #\0 #\9) => #f)
+(check (char-ci>? #\z #\0) => #t)
+(check (char-ci>? #\a #\0) => #t)
+
+;; 边界测试
+(check (char-ci>? #\z #\a) => #t)
+(check (char-ci>? #\Z #\A) => #t)
+(check (char-ci>? #\a #\z) => #f)
+(check (char-ci>? #\A #\Z) => #f)
+
+;; 非字母字符测试
+(check (char-ci>? #\! #\0) => #f)
+(check (char-ci>? #\~ #\0) => #t)
+(check (char-ci>? #\~ #\space) => #t)
+
+;; 错误处理测试 - 更新为 type-error
+(check-catch 'type-error (char-ci>? 1 #\A))
+(check-catch 'type-error (char-ci>? #\A 'symbol))
+(check-catch 'wrong-number-of-args (char-ci>?))
+(check-catch 'wrong-number-of-args (char-ci>? #\A))
+
 (check-report)
 
