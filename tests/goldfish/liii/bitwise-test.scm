0716a53fc75bce109f37239729e1ab54ea4f7646
沈达 <da@liii.pro>
2025-10-20 13:49:38 +0000

    !901 [201_17] 完善 bitwise-and 函数的多参数支持和测试

--- tests/goldfish/liii/bitwise-test.scm
+++ tests/goldfish/liii/bitwise-test.scm
@@ -74,34 +74,39 @@ wrong-type-arg
 
 #|
 bitwise-and
-计算两个整数的按位与操作。
+计算多个整数的按位与操作。
 
 语法
 ----
-(bitwise-and i1 i2)
+(bitwise-and i1 i2 ...)
 
 参数
 ----
-i1 : integer?
-第一个整数，参与按位与操作。
-
-i2 : integer?
-第二个整数，参与按位与操作。
+i1, i2, ... : integer?
+一个或多个整数，参与按位与操作。
 
 返回值
 -----
 integer?
-返回两个整数按位与操作的结果。
+返回所有整数按位与操作的结果。
 
 说明
 ----
-1. 对两个整数的每一位进行与操作（都为1时结果为1，否则为0）
+1. 对所有整数的每一位进行与操作（都为1时结果为1，否则为0）
 2. 按位与操作常用于提取特定位或掩码操作
 3. 对于任意整数 i，(bitwise-and i i) = i
 4. 对于任意整数 i，(bitwise-and i 0) = 0
 5. 对于任意整数 i，(bitwise-and i -1) = i
 6. 按位与操作满足交换律：(bitwise-and i1 i2) = (bitwise-and i2 i1)
 7. 按位与操作满足结合律：(bitwise-and i1 (bitwise-and i2 i3)) = (bitwise-and (bitwise-and i1 i2) i3)
+8. 支持两个或多个参数，按从左到右的顺序依次进行按位与操作
+
+实现说明
+--------
+- bitwise-and 是 SRFI 151 标准定义的函数，提供标准化的位运算接口
+- 如果考虑性能优化，可以使用 S7 Scheme 内置的 logand 函数
+- logand 是 S7 的原生函数，通常比 bitwise-and 有更好的性能
+- bitwise-and支持三个或更多参数，logand仅支持两个参数
 
 错误
 ----
@@ -142,6 +147,18 @@ type-error
 (check (bitwise-and 2147483647 2147483647) => 2147483647) ; 最大32位有符号整数
 (check (bitwise-and -2147483648 -2147483648) => -2147483648) ; 最小32位有符号整数
 (check (bitwise-and 2147483647 -2147483648) => 0) ; 最大和最小整数相与
+
+;;; 三个参数测试
+(check (bitwise-and 1 2 3) => 0)          ; 001 & 010 & 011 = 000
+(check (bitwise-and 7 3 5) => 1)          ; 111 & 011 & 101 = 001
+(check (bitwise-and 15 7 3) => 3)         ; 1111 & 0111 & 0011 = 0011
+(check (bitwise-and #b101 #b011 #b111) => 1) ; 101 & 011 & 111 = 001
+(check (bitwise-and #b1100 #b1010 #b0110) => 0) ; 1100 & 1010 & 0110 = 0000
+(check (bitwise-and 255 127 63) => 63)    ; 11111111 & 01111111 & 00111111 = 00111111
+(check (bitwise-and -1 -1 -1) => -1)      ; 全1相与
+(check (bitwise-and 0 1 2) => 0)          ; 包含0的相与
+(check (bitwise-and 1 1 1) => 1)          ; 全1相与
+(check (bitwise-and 2 2 2) => 2)          ; 相同数相与
 
 ;;; 错误处理测试 - type-error
 (check-catch 'type-error
