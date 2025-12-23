cf0acce4c861d18829b0d1d21bbf4801a4254617
17802723 <he17802723@163.com>
2025-12-23 20:31:42 +0800

    新增返回md5摘要

--- tests/goldfish/liii/hashlib-test.scm
+++ tests/goldfish/liii/hashlib-test.scm
@@ -15,6 +15,7 @@
 ;
 
 (import (liii check) (liii hashlib))
+
 (check (md5 "") => "d41d8cd98f00b204e9800998ecf8427e")
 (check (md5 "hello") => "5d41402abc4b2a76b9719d911017c592")
 (check (md5 "The quick brown fox jumps over the lazy dog") => "9e107d9d372bb6826bd81d3542a419d6")
@@ -22,3 +23,5 @@
 (check (md5 "123456") => "e10adc3949ba59abbe56e057f20f883e")
 (check (md5 "!@#$%^&*()") => "05b28d17a7b6e7024b6e5d8cc43a8bf7")
 (check (md5 "Hello") => "8b1a9953c4611296a827abf8c47804d7")
+
+(check-report)
