4e320227d99ad3f3a7ac12575da21bb2d78eabab
Jinser Kafka <aimer@purejs.icu>
2026-01-22 18:55:49 +0800

    test(liii/http): fix check for case sensitive header

--- tests/goldfish/liii/http-test.scm
+++ tests/goldfish/liii/http-test.scm
@@ -3,20 +3,22 @@
         (liii string)
         (liii rich-json))
 
+(check-set-mode! 'report-failed)
+
 (let1 r (http-head "https://httpbin.org")
   (check (r 'status-code) => 200)
   (check (r 'url) => "https://httpbin.org/")
   (check-true (real? (r 'elapsed)))
-  (check (r 'reason) => "")
+  (check (r 'reason) => "OK")
   (check (r 'text) => "")
-  (check ((r 'headers) "content-type") => "text/html; charset=utf-8")
-  (check ((r 'headers) "content-length") => "9593")
+  (check ((r 'headers) "Content-Type") => "text/html; charset=utf-8")
+  (check ((r 'headers) "Content-Length") => "9593")
   (check-true (http-ok? r)))
 
 (let1 r (http-get "https://httpbin.org")
   (check (r 'status-code) => 200)
   (check-true (> (string-length (r 'text)) 0))
-  (check ((r 'headers) "content-type") => "text/html; charset=utf-8"))
+  (check ((r 'headers) "Content-Type") => "text/html; charset=utf-8"))
 
 (let1 r (http-get "https://httpbin.org/get"
                   :params '(("key1" . "value1") ("key2" . "value2")))
