83dae0c9c8fabc96d06dbb4230a9caa32d975a58
wumo <121778696+17802723@users.noreply.github.com>
2026-01-27 09:26:39 +0800

    [210_7] 实现set的谓词和构造器 (#367)

---
+++ goldfish/liii/set.scm
@@ -0,0 +1,35 @@
+;
+; Copyright (C) 2026 The Goldfish Scheme Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
+(define-library (liii set)
+  (import (rename (srfi srfi-113)
+                  (set make-set-with-comparator)
+                  (list->set list->set-with-comparator))
+          (srfi srfi-128))
+  (export set set-unfold list->set set-copy list->set-with-comparator make-set-with-comparator
+          set? set-contains? set-empty? set-disjoint?
+          set-element-comparator
+          set=? set<? set>? set<=? set>=?)
+  
+  (define comp (make-default-comparator))
+
+  (define (set . elements)
+    (apply make-set-with-comparator comp elements))
+
+  (define (list->set elements)
+    (list->set-with-comparator comp elements))
+  
+) ; end of define-library
