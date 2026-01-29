1adbcff2f4fb9eb7d644234abaa37086b9f1978e
wumo <he17802723@163.com>
2026-01-29 16:59:13 +0800

    wip

--- tests/goldfish/liii/bag-test.scm
+++ tests/goldfish/liii/bag-test.scm
@@ -51,8 +51,7 @@ element ... : any
 (check (bag-member b-empty 1 'missing) => 'missing)
 
 ;; bag->list should include duplicates
-(check-false (not (member 1 b-list)))
-(check-false (not (member 2 b-list)))
+(check b-list  => '(2 2 1))
 (check (length b-list) => 3)
 
 
