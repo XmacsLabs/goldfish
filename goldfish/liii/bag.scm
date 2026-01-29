14309e7d70a9688f301e21c5ac2414a65e357ca9
wumo <he17802723@163.com>
2026-01-29 17:30:33 +0800

    feat: add bag query helpers

--- goldfish/liii/bag.scm
+++ goldfish/liii/bag.scm
@@ -19,11 +19,13 @@
                   (bag make-bag-with-comparator))
           (only (srfi srfi-113)
                 bag-unfold bag-member bag-comparator bag->list
-                bag? bag-contains? bag-empty? bag-disjoint?)
+                bag? bag-contains? bag-empty? bag-disjoint?
+                bag-size bag-find bag-count bag-any? bag-every?)
           (srfi srfi-128))
   (export bag bag-unfold bag-member bag-comparator
           bag->list
-          bag? bag-contains? bag-empty? bag-disjoint?)
+          bag? bag-contains? bag-empty? bag-disjoint?
+          bag-size bag-find bag-count bag-any? bag-every?)
 
   (define comp (make-default-comparator))
 
