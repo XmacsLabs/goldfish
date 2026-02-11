2d36d9c98a959b75d8e396bb0116c68a779bf1b2
jackmath5261-bit <jackmath5261@gmail.com>
2026-02-11 13:41:33 +0800

    [210_13] srfi-19 time-difference实现 (#400)
    * time-difference
    
    * time-difference
    
    * 文档
    
    * 时间类型
    
    ---------
    
    Co-authored-by: JackChen <17683835261@163.com>

--- tests/goldfish/liii/time-test.scm
+++ tests/goldfish/liii/time-test.scm
@@ -339,6 +339,59 @@ wrong-type-arg
   (check (time-nanosecond t3) => 0)
   (check (time-second t3) => -1234567890))
 
+#|
+time-difference
+计算两个时间对象的差值。
+
+语法
+----
+(time-difference time1 time2)
+
+参数
+----
+time1 : time?
+time2 : time?
+两个时间对象，时间类型必须相同。
+
+返回值
+-----
+time?
+返回一个 TIME-DURATION 时间类型的时间对象。
+
+错误处理
+--------
+wrong-type-arg
+当参数不是时间对象或时间类型不匹配时抛出错误。
+|#
+
+;; Test time-difference
+(let* ((t1 (make-time TIME-UTC 100 5))
+       (t2 (make-time TIME-UTC 900000000 3))
+       (d  (time-difference t1 t2)))
+  (check (time-type d) => TIME-DURATION)
+  (check (time-second d) => 1)
+  (check (time-nanosecond d) => 100000100))
+
+;; Test negative duration normalization
+(let* ((t1 (make-time TIME-UTC 100 5))
+       (t2 (make-time TIME-UTC 900000000 5))
+       (d  (time-difference t1 t2)))
+  (check (time-second d) => -1)
+  (check (time-nanosecond d) => 100000100))
+
+;; Test zero difference
+(let* ((t1 (make-time TIME-UTC 123456789 42))
+       (d  (time-difference t1 t1)))
+  (check (time-second d) => 0)
+  (check (time-nanosecond d) => 0))
+
+;; Test error conditions
+(check-catch 'wrong-type-arg
+  (time-difference (make-time TIME-UTC 0 0)
+                   (make-time TIME-TAI 0 0)))
+(check-catch 'wrong-type-arg
+  (time-difference "not-time" (make-time TIME-UTC 0 0)))
+
 ;; Test error conditions
 (check-catch 'wrong-type-arg (time-type "not-a-time"))
 (check-catch 'wrong-type-arg (time-nanosecond 123))
