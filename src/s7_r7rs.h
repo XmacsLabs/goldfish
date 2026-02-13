f6528d01296f1445acb4085865ec3e134d7b91bd
Darcy Shen <da@liii.pro>
2026-02-14 00:23:04 +0800

    [200_21] 将 sqrt 从 s7.c 迁移至 s7_r7rs.c (#421)

--- src/s7_r7rs.h
+++ src/s7_r7rs.h
@@ -15,12 +15,12 @@
 extern "C" {
 #endif
 
-/* R7RS specific symbols */
-extern s7_pointer unlink_symbol, access_symbol, time_symbol, clock_gettime_symbol,
-                  getenvs_symbol, uname_symbol;
+/* Helper function to check for NaN */
+bool is_NaN(s7_double x);
 
 /* R7RS specific function declarations */
-s7_pointer g_getenvs(s7_scheme *sc, s7_pointer args);
+s7_pointer sqrt_p_p(s7_scheme *sc, s7_pointer num);
+s7_pointer g_sqrt(s7_scheme *sc, s7_pointer args);
 
 /* R7RS Scheme code string */
 extern const char r7rs_scm[];
