f2370a4a1a581d71d4e291cf9603be814b81d15a
JackChen <17683835261@163.com>
2026-02-21 23:10:09 +0800

    test

--- tests/goldfish/liii/time-test.scm
+++ tests/goldfish/liii/time-test.scm
@@ -1236,6 +1236,40 @@ string?
 参考：https://srfi.schemers.org/srfi-19/srfi-19.html#:~:text=Table%201,-%3A%20DATE
 |#
 
+;; Test date->string
+(let ((d (make-date 0 0 0 0 1 1 1970 0)))  ; Unix epoch in UTC
+  (check (date->string d)            => "Thu Jan 01 00:00:00Z 1970")
+  (check (date->string d "~Y-~m-~d") => "1970-01-01")
+  (check (date->string d "~H:~M:~S") => "00:00:00"))
+
+;; Test with different dates
+(let ((d1 (make-date 500000000 30 15 9 4 7 1776 0))          ; US Independence
+      (d2 (make-date 123456789 45 30 14 25 12 2023 28800))   ; Christmas in UTC+8
+      (d3 (make-date 999999999 59 59 23 31 12 1999 -18000))) ; Y2K in UTC-5
+  (check (date->string d1)                             => "Thu Jul 04 09:15:30Z 1776")
+  (check (date->string d2 "~Y-~m-~d ~H:~M:~S")         => "2023-12-25 14:30:45")
+  (check (date->string d3 "~A, ~B ~d, ~Y ~I:~M:~S ~p") => "Friday, December 31, 1999 11:59:59 PM"))
+
+;; Test format specifiers
+(let ((d (make-date 123456789 45 30 14 25 12 2023 28800)))
+  ;; Basic numeric formats
+  (check (date->string d "~Y-~m-~d ~H:~M:~S") => "2023-12-25 14:30:45")
+
+  ;; Test literal ~
+  (check (date->string d "~~") => "~")
+
+  ;; Test combination of specifiers and literals
+  (check (date->string d "Date: ~Y/~m/~d Time: ~H:~M") => "Date: 2023/12/25 Time: 14:30")
+
+  ;; Test CJK
+  (check (date->string d "~Y年，第~V週。~H시~M분") => "2023年，第52週。14시30분"))
+
+  ;; Test error conditions
+(let ((d (make-date 0 0 0 0 1 1 1970 0)))
+  (check-catch 'wrong-type-arg (date->string "not-a-date"))
+  (check-catch 'wrong-type-arg (date->string d 123))  ; format-string not a string
+  (check-catch 'wrong-type-arg (date->string d 'symbol)))
+
 #|
 string->date
 将字符串按模板解析为日期对象。
@@ -1272,34 +1306,6 @@ wrong-type-arg
 value-error
 当输入字符串无法匹配模板时抛出错误。
 |#
-
-;; Test date->string
-(let ((d (make-date 0 0 0 0 1 1 1970 0)))  ; Unix epoch in UTC
-  (check (date->string d)            => "Thu Jan 01 00:00:00Z 1970")
-  (check (date->string d "~Y-~m-~d") => "1970-01-01")
-  (check (date->string d "~H:~M:~S") => "00:00:00"))
-
-;; Test with different dates
-(let ((d1 (make-date 500000000 30 15 9 4 7 1776 0))          ; US Independence
-      (d2 (make-date 123456789 45 30 14 25 12 2023 28800))   ; Christmas in UTC+8
-      (d3 (make-date 999999999 59 59 23 31 12 1999 -18000))) ; Y2K in UTC-5
-  (check (date->string d1)                             => "Thu Jul 04 09:15:30Z 1776")
-  (check (date->string d2 "~Y-~m-~d ~H:~M:~S")         => "2023-12-25 14:30:45")
-  (check (date->string d3 "~A, ~B ~d, ~Y ~I:~M:~S ~p") => "Friday, December 31, 1999 11:59:59 PM"))
-
-;; Test format specifiers
-(let ((d (make-date 123456789 45 30 14 25 12 2023 28800)))
-  ;; Basic numeric formats
-  (check (date->string d "~Y-~m-~d ~H:~M:~S") => "2023-12-25 14:30:45")
-
-  ;; Test literal ~
-  (check (date->string d "~~") => "~")
-
-  ;; Test combination of specifiers and literals
-  (check (date->string d "Date: ~Y/~m/~d Time: ~H:~M") => "Date: 2023/12/25 Time: 14:30")
-
-  ;; Test CJK
-  (check (date->string d "~Y年，第~V週。~H시~M분") => "2023年，第52週。14시30분"))
 
 ;; Roundtrip date->string -> string->date
 (let* ((d (make-date 0 45 30 14 25 12 2023 28800))
@@ -1366,12 +1372,6 @@ value-error
 (check-catch 'wrong-type-arg (string->date 1 "~Y"))
 (check-catch 'wrong-type-arg (string->date "2020" 123))
 (check-catch 'value-error (string->date "2020-01-01" "~Y/~m/~d"))
-
-;; Test error conditions
-(let ((d (make-date 0 0 0 0 1 1 1970 0)))
-  (check-catch 'wrong-type-arg (date->string "not-a-date"))
-  (check-catch 'wrong-type-arg (date->string d 123))  ; format-string not a string
-  (check-catch 'wrong-type-arg (date->string d 'symbol)))
 
 #|
 current-date
